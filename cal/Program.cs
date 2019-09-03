6a404278d4418c9ef52ca11a9a2ac24b64a1e7ba
Dinuka Sankalpa <44248646+dinuka10@users.noreply.github.com>
2019-09-03 09:08:20 +0530

    Add files via upload

---
+++ cal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace cal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number 1 : ");
+            
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter number 2 : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(" Enter one of these operations ::  "+ " (+)"+" (-)"+" (x) "+"(/)");
+            string op = Console.ReadLine();
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1+num2);
+            }
+            if (op == "-")
+            {
+                Console.WriteLine(num1-num2);
+            }
+            if (op == "x")
+            {
+                Console.WriteLine(num1*num2);
+            }
+            if (op == "/")
+            {
+                Console.WriteLine(num1/num2);
+
+            }
+                Console.ReadLine();
+        }
+    }
+}
